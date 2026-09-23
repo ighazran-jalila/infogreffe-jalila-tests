33af7cc2562d9376837c261671fcd6d045485589
Jalila <ighazran.jalila@gmail.com>
2026-09-23 16:57:10 +0100

    Refactor POM: add .env support, update locators and Act-Logic-Assert structure

--- tests/infogreffe.spec.ts
+++ tests/infogreffe.spec.ts
@@ -3,7 +3,7 @@ import { InfogreffePage } from '../pages/infogreffe.page';
 
 test.beforeEach(async ({ page }) => {
   const infogreffePage = new InfogreffePage(page);
- // 1. ACT 
+ //  ACT 
     await infogreffePage.navigateTo();
     await infogreffePage.acceptCookies();
     await infogreffePage.openLoginForm();
@@ -15,26 +15,26 @@ test.describe('Parcours Recherche Infogreffe', () => {
 
   test('Search enterprise "PAUL", open "SCI SAINT PAUL" and verify "Actes et statuts"', async ({ page }) => {
     const infogreffePage = new InfogreffePage(page);
-    // 2. LOGIC 
+    // LOGIC 
     await infogreffePage.selectAccountIfAppears();
     await infogreffePage.clickAndSearch('PAUL');
     await infogreffePage.selectSciSaintPaul();
     await infogreffePage.scrollToActesEtStatuts();
-    // 3. ASSERT 
+    //  ASSERT 
     await expect(infogreffePage.getEnterpriseTitleLocator()).toContainText('SCI SAINT PAUL');
     await expect(infogreffePage.getSirenLocator()).toBeVisible()
     await expect(infogreffePage.getActesEtStatutsSectionLocator()).toBeVisible();
   });
   test('Commander Extrait Kbis pour "Carrefour"', async ({ page }) => {
         const infogreffePage = new InfogreffePage(page);
-  // 1. LOGIC
+  //  LOGIC
       await infogreffePage.selectAccountIfAppears();
       await infogreffePage.goToKbisDocuments();
       await infogreffePage.clickOrderKbis();
       await infogreffePage.searchKbisEnterprise('Carrefour');
       await infogreffePage.selectCarrefourEnterprise();
       await infogreffePage.scrollToBottomAndKbis();
-  // 2. ASSERT (Vérifications explicites)
+  //  ASSERT (Vérifications explicites)
       await expect(infogreffePage.getCarrefourTitleLocator()).toContainText('CARREFOUR');
       await expect(infogreffePage.getKbisSectionLocator()).toBeVisible();
 });
